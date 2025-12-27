27d67b3a299db2b510413fe53233602bf8b044eb
tyun <joker1618@livemail.tw>
2025-12-26 21:45:05 -0800

    feat(map): reap loop

--- pkg/storage/map.go
+++ pkg/storage/map.go
@@ -55,16 +55,16 @@ func (s *ShardedMap) getShard(key string) *shard {
 // Register a new service (write)
 func (s *ShardedMap) Register(serviceName, endpoint string, ttl int64) {
 
-	shard := s.getShard(serviceName)
+	sh := s.getShard(serviceName)
 
-	shard.Lock()
-	defer shard.Unlock()
+	sh.Lock()
+	defer sh.Unlock()
 
-	if _, ok := shard.m[serviceName]; !ok {
-		shard.m[serviceName] = make(map[string]*InstanceInfo)
+	if _, ok := sh.m[serviceName]; !ok {
+		sh.m[serviceName] = make(map[string]*InstanceInfo)
 	}
 
-	shard.m[serviceName][endpoint] = &InstanceInfo{
+	sh.m[serviceName][endpoint] = &InstanceInfo{
 		ServiceName: serviceName,
 		Endpoint: endpoint,
 		LastHeartbeat: time.Now().Unix(),
@@ -73,17 +73,44 @@ func (s *ShardedMap) Register(serviceName, endpoint string, ttl int64) {
 
 // GetEndpoints (read)
 func (s *ShardedMap) GetEndpoints(serviceName string) []string {
-	shard := s.getShard(serviceName)
-	shard.RLock()
-	defer shard.RUnlock()
+	sh := s.getShard(serviceName)
+	sh.RLock()
+	defer sh.RUnlock()
 
-	// init a String Slice
+	// init a String Slice (empty slice. cap = 0)
 	endpoints := make([]string, 0)
 
-	if instances, ok := shard.m[serviceName]; ok {
+	if instances, ok := sh.m[serviceName]; ok {
 		for ep := range instances {
 			endpoints = append(endpoints, ep)
 		}
 	}
 	return endpoints
+}
+
+// ReapLoop goroutine: clear timeout service instance
+func (s *ShardedMap) ReapLoop(ttlSeconds int64) {
+	for _, item := range s.shards {
+		go func(sh *shard) {
+			ticker := time.NewTicker(1 * time.Second)
+
+			for range ticker.C {
+				// write lock for a single shard
+				sh.Lock()
+
+				now := time.Now().Unix()
+				for serviceName, infos := range sh.m {
+					for endpoint, instanceInfo := range infos {
+						if now-instanceInfo.LastHeartbeat > ttlSeconds {
+							delete(infos, endpoint)
+						}
+					}
+					if len(infos) == 0 {
+						delete(sh.m, serviceName)
+					}
+				}
+				sh.Unlock()
+			}
+		}(item)
+	}
 }
