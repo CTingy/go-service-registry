d3b86043daca244cf6a85890f8ab6fa81b6a4e5a
tyun <joker1618@livemail.tw>
2025-12-28 18:59:14 -0800

    feat(server): add registry server

--- server/handler.go
+++ server/handler.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"go-service-registry/pkg/storage"
+	pb "go-service-registry/proto"
+)
+
+type RegistryServer struct {
+	pb.UnimplementedRegistryServer
+	store      *storage.ShardedMap
+	defaultTTL int64
+}
+
+func NewRegistryServer(store *storage.ShardedMap, ttl int64) *RegistryServer {
+	return &RegistryServer{
+		store:      store,
+		defaultTTL: ttl,
+	}
+}
