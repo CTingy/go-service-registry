c7c8095386390a4d4b973fe7b2f94a2ed0ff9773
tyun <joker1618@livemail.tw>
2026-01-06 14:14:27 -0800

    feat(handler): add RegistryServer interface methods

--- server/handler.go
+++ server/handler.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"go-service-registry/pkg/storage"
 	pb "go-service-registry/proto"
 )
@@ -16,4 +17,25 @@ func NewRegistryServer(store *storage.ShardedMap, ttl int64) *RegistryServer {
 		store:      store,
 		defaultTTL: ttl,
 	}
+}
+
+/*
+type RegistryServer interface {
+	Register(context.Context, *RegisterReq) (*RegisterResp, error)
+	Heartbeat(context.Context, *HeartbeatReq) (*Empty, error)
+	Discover(context.Context, *DiscoverReq) (*DiscoverResp, error)
+	mustEmbedUnimplementedRegistryServer()
+}
+*/
+
+func (s *RegistryServer) Register(ctx context.Context, req *pb.RegisterReq) (*pb.RegisterResp, error) {
+	return &pb.RegisterResp{}, nil
+}
+
+func (s *RegistryServer) Heartbeat(ctx context.Context, req *pb.HeartbeatReq) (*pb.Empty, error) {
+	return &pb.Empty{}, nil
+}
+
+func (s *RegistryServer) Discover(ctx context.Context, req *pb.DiscoverReq) (*pb.DiscoverResp, error) {
+	return &pb.DiscoverResp{}, nil
 }
