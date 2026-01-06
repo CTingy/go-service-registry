c3d5f5fd08ca8aea5543607e7d77a86a47236d95
tyun <joker1618@livemail.tw>
2026-01-06 14:59:21 -0800

    feat(client): create grpc connection

--- client/main.go
+++ client/main.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	"google.golang.org/grpc"
+	// build non-encryption connection
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	// 1. set parameters
+	// flag.String(param name, default value, help text)
+	// command: go run client/main.go -name=payment-service -port=8081
+	serviceName := flag.String("name", "auth-service", "Service Name")
+	port := flag.String("port", "8080", "Service port")
+	flag.Parse()
+
+	// construct client's address
+	addr := fmt.Sprintf("127.0.0.1:%s", *port)
+
+	// 2. connect to Registry Server
+	target := "127.0.0.1:50055"
+	conn, err := grpc.NewClient(target, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+}
